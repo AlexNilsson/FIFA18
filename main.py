69b2920a3081efec04170ed24e5ce5082641dc23
alexnilsson <alexandernilsson12@gmail.com>
2018-06-13 17:28:46 +0200

    ready set go

--- main.py
+++ main.py
@@ -2,25 +2,22 @@ import os
 
 import numpy as np
 
-from keras.callbacks import ModelCheckpoint
-from keras.layers import Input, Dense, Dropout
-from keras.layers import BatchNormalization, Activation
-from keras.layers.advanced_activations import LeakyReLU
-from keras.models import Sequential, Model
-from keras.optimizers import Adam
-
-import keras.backend as K
-
 from utility import csv_to_dict, keep_entries, group_by_key, unique_members_from_columns
+import config as C
+
+from FIFANET import FIFANET
 
 """ FILE PATHS """
 PATH_TO_ROOT = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_DATA = os.path.join(PATH_TO_ROOT, 'data')
+PATH_TO_PREDICT = os.path.join(PATH_TO_ROOT, 'predict')
 
 """ LOAD DATA """
-WORLD_CUPS        = csv_to_dict(os.path.join(PATH_TO_DATA, 'WorldCups.csv')) 
-WORLD_CUP_PLAYERS = csv_to_dict(os.path.join(PATH_TO_DATA, 'WorldCupPlayers.csv')) 
+WORLD_CUPS        = csv_to_dict(os.path.join(PATH_TO_DATA, 'WorldCups.csv'))
+WORLD_CUP_PLAYERS = csv_to_dict(os.path.join(PATH_TO_DATA, 'WorldCupPlayers.csv'))
 WORLD_CUP_MATCHES = csv_to_dict(os.path.join(PATH_TO_DATA, 'WorldCupMatches.csv'))
+
+PREDICT_MATCHES = csv_to_dict(os.path.join(PATH_TO_PREDICT, 'matches.csv'))
 
 """ PREPROCESS DATA"""
 # EXTRACT FEATURES OF INTEREST
@@ -33,15 +30,39 @@ ALL_TEAMS =  unique_members_from_columns(WORLD_CUP_MATCHES, ['Home Team Name', '
 ALL_TEAMS_ENCODING = dict(zip(ALL_TEAMS, np.eye(len(ALL_TEAMS))))
 
 # GROUP BY YEAR/CUP
-
 WORLD_CUP_MATCHES = group_by_key(WORLD_CUP_MATCHES, 'Year')
 
+# Decay scores to account for athletic improvements over time
+N_GAMES = len(WORLD_CUP_MATCHES)
+for i, year in enumerate(WORLD_CUP_MATCHES):
+  scoreDecay = 1 - C.SCORE_DECAY_FACTOR * (N_GAMES-i-1)/(N_GAMES-1)
+  for i, match in enumerate(WORLD_CUP_MATCHES[year]):
+    try:
+      match[2] = scoreDecay * int(match[2])
+      match[3] = scoreDecay * int(match[2])
+    except:
+      # Remove matches written in the wrong format in the database
+      # Some entries have home team & scores mixed up and can not be trusted :(
+      del match
+
+# Matches to Predict
+temp = []
+for x in PREDICT_MATCHES:
+  teams = list(x.values())
+  match = []
+
+  temp.append(list(np.append(
+    ALL_TEAMS_ENCODING[teams[0]],
+    ALL_TEAMS_ENCODING[teams[1]]
+  )))
+
+PREDICT_MATCHES = np.array(temp, dtype=int)
 
 """ SPLIT DATA INTO TRAIN/TEST SETS """
 """ AND SEPARATE FEATURES FROM LABELS """
-TEST_YEAR = '2014'
-
-TEST_DATA_RAW = WORLD_CUP_MATCHES.pop(TEST_YEAR)
+if C.USE_TEST_DATA:
+  TEST_YEAR = '2014'
+  TEST_DATA_RAW = WORLD_CUP_MATCHES.pop(TEST_YEAR)
 
 TRAIN_DATA_RAW = []
 for x in WORLD_CUP_MATCHES:
@@ -49,120 +70,63 @@ for x in WORLD_CUP_MATCHES:
     TRAIN_DATA_RAW.append(m)
 
 
-TRAIN_DATA = [[],[]]
+TRAIN_DATA_FEATURES = []
+TRAIN_DATA_LABELS = []
 for x in TRAIN_DATA_RAW:
   try:
-    np.array([x[2],x[3]],dtype=int)
+    np.array([x[2],x[3]],dtype=float)
 
-    combined = np.append(
-      np.array(ALL_TEAMS_ENCODING[x[0]],dtype=int),
-      np.array(ALL_TEAMS_ENCODING[x[1]],dtype=int)
-    )
-    TRAIN_DATA[0].append(combined)
+    combined = np.append( ALL_TEAMS_ENCODING[x[0]], ALL_TEAMS_ENCODING[x[1]] )
+    TRAIN_DATA_FEATURES.append(list(combined))
 
-    TRAIN_DATA[1].append(
-      np.array([x[2], x[3]], dtype=int)
+    TRAIN_DATA_LABELS.append(
+      list(np.array([x[2], x[3]], dtype=float))
     )
+
   except: continue
 
-TRAIN_DATA = np.array(TRAIN_DATA)
+TRAIN_DATA_FEATURES = np.array(TRAIN_DATA_FEATURES, dtype=int)
+TRAIN_DATA_LABELS = np.array(TRAIN_DATA_LABELS, dtype=float)
 
+if C.USE_TEST_DATA:
+  TEST_DATA_FEATURES = []
+  TEST_DATA_LABELS = []
+  for x in TEST_DATA_RAW:
+    try:
+      np.array([x[2],x[3]],dtype=float)
 
-TEST_DATA = [[],[]]
-for x in TEST_DATA_RAW:
-  try:
-    np.array([x[2],x[3]],dtype=int)
+      combined = np.append(ALL_TEAMS_ENCODING[x[0]], ALL_TEAMS_ENCODING[x[1]])
+      TEST_DATA_FEATURES.append(list(combined))
 
-    combined = np.append(
-        np.array(ALL_TEAMS_ENCODING[x[0]],dtype=int),
-        np.array(ALL_TEAMS_ENCODING[x[1]],dtype=int)
-    )
+      TEST_DATA_LABELS.append(
+        list(np.array([x[2], x[3]], dtype=float))
+      )
+    except: continue
 
-    TEST_DATA[0].append(combined)
-
-    TEST_DATA[1].append(
-      np.array([x[2], x[3]], dtype=int)
-    )
-  except: continue
-
-TEST_DATA = np.array(TEST_DATA)
-
-class FIFANET():
-  def __init__(self):
-    self.input_shape = (186,1)
-    self.output_shape = (2,1)
-    self.dropout_amount = 0.2
-    self.optimizer = Adam(lr=0.0001)
-
-    self.model = self.buildNetwork()
-    self.model.compile(loss='mean_squared_error', optimizer=self.optimizer)
-
-  def buildNetwork(self):
-    model = Sequential()
-
-    model.add(Dense(512, input_dim=186))
-    #model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
-    model.add(Dropout(self.dropout_amount))
-    
-    model.add(Dense(256))
-    #model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
-    model.add(Dropout(self.dropout_amount))
-
-    model.add(Dense(128))
-    #model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
-    model.add(Dropout(self.dropout_amount))
-
-    model.add(Dense(64))
-    #model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
-    model.add(Dropout(self.dropout_amount))
-
-    model.add(Dense(32))
-    #model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
-    model.add(Dropout(self.dropout_amount))
-
-    model.add(Dense(16))
-    #model.add(BatchNormalization(momentum=0.8))
-    model.add(Activation("relu"))
-    model.add(Dropout(self.dropout_amount))
-
-    model.add(Dense(2, activation="relu"))
-
-    model.summary()
-
-    return model
-
-  def train(self, x_train, y_train, x_test, y_test, epochs=100, batch_size=128):
-    print(x_train.shape)
-    print(x_train[0])
-    print(x_train[1])
-    print(x_train[2])
-
-
-    self.model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
-    score = self.model.evaluate(x_test, y_test, batch_size=batch_size)
-    print('test score: {}'.format(score))
-
+  TEST_DATA_FEATURES = np.array(TEST_DATA_FEATURES, dtype=int)
+  TEST_DATA_LABELS = np.array(TEST_DATA_LABELS, dtype=float)
 
 """ EXECUTE """
 fifaNet = FIFANET()
-fifaNet.train(
-  np.random.random((1000, 186)),
-  np.random.randint(2, size=(1000, 2)),
-  np.random.random((1000, 186)),
-  np.random.randint(2, size=(1000, 2)),
-  epochs = 1000,
-  batch_size = 10
-)
-""" fifaNet.train(
-  np.array(TRAIN_DATA[0]),
-  np.array(TRAIN_DATA[1]),
-  np.array(TEST_DATA[0]),
-  np.array(TEST_DATA[1]),
-  epochs = 1000,
-  batch_size = 10
-) """
+
+if C.USE_TEST_DATA:
+  fifaNet.train(
+    np.array(TRAIN_DATA_FEATURES),
+    np.array(TRAIN_DATA_LABELS),
+    np.array(TEST_DATA_FEATURES),
+    np.array(TEST_DATA_LABELS),
+    epochs = C.EPOCHS,
+    batch_size = C.BATCH_SIZE
+  )
+
+else:
+  fifaNet.train(
+    np.array(TRAIN_DATA_FEATURES),
+    np.array(TRAIN_DATA_LABELS),
+    np.array(TRAIN_DATA_FEATURES),
+    np.array(TRAIN_DATA_LABELS),
+    epochs = C.EPOCHS,
+    batch_size = C.BATCH_SIZE
+  )
+
+fifaNet.predict(np.array(PREDICT_MATCHES))
